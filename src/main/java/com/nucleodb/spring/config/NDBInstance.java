f44970cbab7c5431d39ccf0449073a4f3c6aad61
Nathaniel Davidson <nathaniel.davidson@gmail.com>
2024-12-12 21:12:27 -0800

    add NDBMappingContext bean

--- src/main/java/com/nucleodb/spring/config/NDBInstance.java
+++ src/main/java/com/nucleodb/spring/config/NDBInstance.java
@@ -1,13 +1,20 @@
 package com.nucleodb.spring.config;
 
 import com.nucleodb.library.NucleoDB;
+import com.nucleodb.spring.mapping.NDBMappingContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class NDBInstance {
     @Bean
     public NucleoDB createNucleoDB(){
         return new NucleoDB();
+    }
+    @Bean
+    @Primary
+    public NDBMappingContext ndbMappingContext() {
+        return new NDBMappingContext();
     }
 }
